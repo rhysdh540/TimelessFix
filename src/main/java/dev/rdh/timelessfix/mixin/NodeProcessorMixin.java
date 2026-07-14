00c2850e0b286231c7b03f16fb0d926bbc44e615
rdh <me@rdh540.dev>
2026-07-14 11:49:59 +0530

    Release pathfinder block access after use

---
+++ src/main/java/dev/rdh/timelessfix/mixin/NodeProcessorMixin.java
@@ -0,0 +1,19 @@
+package dev.rdh.timelessfix.mixin;
+
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.pathfinder.NodeProcessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NodeProcessor.class)
+abstract class NodeProcessorMixin {
+	@Shadow protected IBlockAccess blockaccess;
+
+	@Inject(method = "postProcess", at = @At("HEAD"))
+	private void releaseBlockAccess(CallbackInfo ci) {
+		this.blockaccess = null;
+	}
+}
