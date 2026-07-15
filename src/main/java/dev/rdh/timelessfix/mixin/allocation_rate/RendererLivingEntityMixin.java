45d30eddd7f483b86172fad18d316aad1d764b1f
rdh <me@rdh540.dev>
2026-07-16 04:56:23 +0530

    Avoid entity name component allocations

--- src/main/java/dev/rdh/timelessfix/mixin/allocation_rate/RendererLivingEntityMixin.java
+++ src/main/java/dev/rdh/timelessfix/mixin/allocation_rate/RendererLivingEntityMixin.java
@@ -3,12 +3,37 @@ package dev.rdh.timelessfix.mixin.allocation_rate;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RendererLivingEntity.class)
 abstract class RendererLivingEntityMixin {
+	@Unique private static final IChatComponent timelessFix$emptyName = new ChatComponentText("");
+	@Unique private String timelessFix$renderedName;
+
+	@Redirect(
+		method = "renderName",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;getDisplayName()Lnet/minecraft/util/IChatComponent;")
+	)
+	private IChatComponent prepareRenderedName(EntityLivingBase entity) {
+		String name = entity.getName();
+		this.timelessFix$renderedName = entity instanceof EntityPlayer ? ScorePlayerTeam.formatPlayerName(entity.getTeam(), name) : name;
+		return timelessFix$emptyName;
+	}
+
+	@Redirect(
+		method = "renderName",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/util/IChatComponent;getFormattedText()Ljava/lang/String;")
+	)
+	private String usePreparedName(IChatComponent ignored) {
+		return this.timelessFix$renderedName;
+	}
+
 	@Redirect(
 		method = "rotateCorpse",
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;getName()Ljava/lang/String;")
